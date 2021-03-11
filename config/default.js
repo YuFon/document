8db590420ea9dc892692ba52ec14d2d2707ec595
YuFon <506184177@qq.com>
2021-03-11 20:52:39 +0800

    mongo init

--- config/default.js
+++ config/default.js
@@ -5,5 +5,5 @@ module.exports = {
       key: 'myblog',
       maxAge: 2592000000
     },
-    mongodb: 'mongodb://localhost:27017/myblog' // mongodb 的地址，以 `mongodb://` 协议开头，`myblog` 为 db 名
+    mongodb: 'mongodb://localhost:27017/document' // mongodb 的地址，以 `mongodb://` 协议开头，`myblog` 为 db 名
 }
